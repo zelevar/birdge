d70551ab58381366a65afcb93fb589731f7a1505
zelevar <zelevarx@proton.me>
2025-01-13 23:58:31 +0300

    Rewrite

--- impl.py
+++ impl.py
@@ -1,8 +1,14 @@
 from dataclasses import dataclass
-from enum import IntEnum
+from enum import Enum
+from socket import AF_INET, SOCK_DGRAM, socket
+from typing import Self
+
+Address = tuple[str, int]
+SOURCE_PORT = 2025
+MAX_PACKET_SIZE = 65507
 
 
-class PacketType(IntEnum):
+class PacketType(Enum):
     CONNECT = 0
     ACCEPT = 1
 
@@ -15,3 +21,78 @@ class Packet:
     # TODO: find a ready-made lib that would do the following out of the box
     def pack(self) -> bytes:
         return self.type.value.to_bytes(1) + self.payload
+    
+    @classmethod
+    def unpack(cls, data: bytes) -> Self:
+        return cls(
+            type=PacketType(int.from_bytes(data[:1])),
+            payload=data[1:],
+        )
+    
+
+# TODO: split this to the files
+    
+
+class SessionState(Enum):
+    DISCONNECTED = 0
+    CONNECTED = 1
+
+
+class MissingPeerError(Exception):
+    ...
+
+
+class HandshakeError(Exception):
+    ...
+
+
+class Session:
+    state: SessionState = SessionState.DISCONNECTED
+    peer: Address | None = None
+    
+    def __init__(self, port: int = 2025) -> None:
+        self.port = port
+
+        self.socket = socket(AF_INET, SOCK_DGRAM)
+        self.socket.bind(('0.0.0.0', self.port))
+        self.socket.settimeout(30)
+
+    def send_packet(self, packet: Packet) -> None:
+        if not self.peer:
+            raise MissingPeerError()
+    
+        data = packet.pack()
+        self.socket.sendto(data, self.peer)
+    
+    def receive_packet(self) -> Packet:
+        if not self.peer:
+            raise MissingPeerError()
+        
+        data, addr = self.socket.recvfrom(MAX_PACKET_SIZE)
+        if addr != self.peer:
+            # TODO: add warning
+            # FIXME: recursion limit vulnerability
+            return self.receive_packet()
+        
+        return Packet.unpack(data)
+    
+    def _establish(self) -> None:
+        self.state = SessionState.CONNECTED
+    
+    def connect(self, peer: Address) -> None:
+        # if already connected
+        if self.state == SessionState.CONNECTED:
+            # TODO: raise an exception
+            pass
+        
+        self.peer = peer
+        self.send_packet(Packet(PacketType.CONNECT))
+
+        packet = self.receive_packet()
+        if packet.type == PacketType.CONNECT:
+            self.send_packet(Packet(PacketType.ACCEPT))
+            self._establish()
+        elif packet.type == PacketType.ACCEPT:
+            self._establish()
+        else:
+            raise HandshakeError(f"incorrect incoming packet during handshake ({packet.type})")
--- main.py
+++ main.py
@@ -1,17 +1,14 @@
-from socket import AF_INET, SOCK_DGRAM, socket
-
-from impl import Packet, PacketType
+from impl import Session
 from utils import address_to_code, code_to_address, get_external_address
 
-sock = socket(AF_INET, SOCK_DGRAM)
+session = Session()
 
-my_addr = get_external_address(sock)
+my_addr = get_external_address(session.socket)
 my_code = address_to_code(my_addr)
 print(f"Your code: {my_code}")
 
 peer_code = input("Peer's code: ")
 peer_addr = code_to_address(peer_code)
 
-sock.sendto(Packet(PacketType.CONNECT).pack(), peer_addr)
-test = sock.recvfrom(65507)
-print(test[1], len(test[0]), test[0])
+session.connect(peer_addr)
+print("Connected!")
